64ce52c24f6092faf6de2cc26c79d3e6fd71b138
bradpreston <bap5393@gmail.com>
2024-08-04 16:07:11 -0400

    Inject PrismaService into EntriesService

--- src/entries/entries.service.ts
+++ src/entries/entries.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEntryDto } from './dto/create-entry.dto';
 import { UpdateEntryDto } from './dto/update-entry.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class EntriesService {
+  constructor(private prisma: PrismaService) {}
+
   create(createEntryDto: CreateEntryDto) {
     return 'This action adds a new entry';
   }
