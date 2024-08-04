085148a5250044747e4e284c4b11ace08d8f8b0c
bradpreston <bap5393@gmail.com>
2024-08-04 15:20:01 -0400

    Created an initial migration

---
+++ prisma/migrations/20240804191836_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Entry" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "teamName" TEXT NOT NULL,
+    "additionalEmails" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Entry_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Entry_teamName_key" ON "Entry"("teamName");
