50fd83d3a210a581667ea04245a3457dbb1b5a12
bradpreston <bap5393@gmail.com>
2024-08-04 15:40:26 -0400

    Setup Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Kazoo 48 Film Festival')
+    .setDescription(
+      'API documentation for the Kazoo 48 Film Festival admin dashboard',
+    )
+    .setVersion('0.1')
+    .build();
+  // Swagger document setup
+  const document = SwaggerModule.createDocument(app, config);
+  // setup Swagger with the enpoint /api
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
