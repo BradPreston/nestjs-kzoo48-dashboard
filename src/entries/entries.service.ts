42f6b27b73b5eb2a8262189fc693fce8c0ad3610
bradpreston <bap5393@gmail.com>
2024-08-04 16:44:17 -0400

    Add error handling in the EntriesService

--- src/entries/entries.service.ts
+++ src/entries/entries.service.ts
@@ -1,22 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateEntryDto } from './dto/create-entry.dto';
 import { UpdateEntryDto } from './dto/update-entry.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class EntriesService {
   constructor(private prisma: PrismaService) {}
 
-  create(createEntryDto: CreateEntryDto) {
-    return 'This action adds a new entry';
+  async create(createEntryDto: CreateEntryDto) {
+    try {
+      return await this.prisma.entry.create({ data: createEntryDto });
+    } catch (err) {
+      if (err instanceof Prisma.PrismaClientValidationError) {
+        // TODO: cleanup this message, this is a little 'hacky'
+        const createErrorMessage = err.message.split('\n');
+        throw new BadRequestException(
+          createErrorMessage[createErrorMessage.length - 1],
+        );
+      }
+      // if error is not from a client validation issue
+      throw new BadRequestException(err.message);
+    }
   }
 
   findAll() {
     return this.prisma.entry.findMany();
   }
 
-  findOne(id: number) {
-    return this.prisma.entry.findUnique({ where: { id } });
+  async findOne(id: number) {
+    const entry = await this.prisma.entry.findUnique({ where: { id } });
+    if (!entry)
+      throw new NotFoundException(`No user with id "${id}" was found`);
+    return entry;
   }
 
   update(id: number, updateEntryDto: UpdateEntryDto) {
