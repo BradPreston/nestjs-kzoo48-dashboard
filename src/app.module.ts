e44705e0da57fcfd47600dd9291aa4c5a1342a76
bradpreston <bap5393@gmail.com>
2024-08-04 15:48:59 -0400

    Generate REST API endpoints using 'nest generate resouce'

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { EntriesModule } from './entries/entries.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, EntriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
