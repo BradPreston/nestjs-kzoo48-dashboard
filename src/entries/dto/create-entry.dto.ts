30fde72d5a7811bd91c85a4bd498cc873c1554dc
bradpreston <bap5393@gmail.com>
2024-08-04 16:43:26 -0400

    Add class validators to CreateEntryDto

--- src/entries/dto/create-entry.dto.ts
+++ src/entries/dto/create-entry.dto.ts
@@ -1,1 +1,44 @@
-export class CreateEntryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsArray,
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
+
+export class CreateEntryDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @ApiProperty()
+  @IsPhoneNumber()
+  @IsNotEmpty()
+  phone: string;
+
+  @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  category: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  teamName: string;
+
+  @ApiProperty()
+  @IsArray()
+  additionalEmails: string[];
+}
