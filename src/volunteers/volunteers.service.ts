dac1d3394c327070f5c16a256ac111ae38361151
brad preston <bap5393@gmail.com>
2024-08-11 15:15:29 -0400

    Update volunteer service to remove one volunteer from the db

--- src/volunteers/volunteers.service.ts
+++ src/volunteers/volunteers.service.ts
@@ -1,7 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateVolunteerDto } from './dto/create-volunteer.dto';
 import { UpdateVolunteerDto } from './dto/update-volunteer.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class VolunteersService {
@@ -26,7 +31,14 @@ export class VolunteersService {
     return `This action updates a #${id} volunteer`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} volunteer`;
+  async remove(id: number) {
+    try {
+      return await this.prisma.volunteer.delete({ where: { id } });
+    } catch (err) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new NotFoundException(`No volunteer with id "${id}" was found`);
+      }
+      throw new BadRequestException('Something went wrong');
+    }
   }
 }
