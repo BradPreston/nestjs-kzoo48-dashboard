180d3278318cd9a88778fe801182da78802d4b02
brad preston <bap5393@gmail.com>
2024-08-11 15:10:30 -0400

    Import PrismaModule into volunteers module

--- src/volunteers/volunteers.module.ts
+++ src/volunteers/volunteers.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VolunteersService } from './volunteers.service';
 import { VolunteersController } from './volunteers.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [VolunteersController],
   providers: [VolunteersService],
+  imports: [PrismaModule],
 })
 export class VolunteersModule {}
