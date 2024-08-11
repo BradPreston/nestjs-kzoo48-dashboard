7d82ce12d3860da73eb4700b788f1dee9a1acfcb
bradpreston <bap5393@gmail.com>
2024-08-11 10:22:04 -0400

    Generate volunteers REST endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { EntriesModule } from './entries/entries.module';
+import { VolunteersModule } from './volunteers/volunteers.module';
 
 @Module({
-  imports: [PrismaModule, EntriesModule],
+  imports: [PrismaModule, EntriesModule, VolunteersModule],
   controllers: [AppController],
   providers: [AppService],
 })
