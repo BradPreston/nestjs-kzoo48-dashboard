e391a0c7bc9a8b64b6c0d6651f0612a648173fd1
bradpreston <bap5393@gmail.com>
2024-08-15 21:26:04 -0400

    Add payment module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { EntriesModule } from './entries/entries.module';
 import { VolunteersModule } from './volunteers/volunteers.module';
+import { PaymentsModule } from './payments/payment.module';
 
 @Module({
-  imports: [PrismaModule, EntriesModule, VolunteersModule],
+  imports: [PrismaModule, EntriesModule, VolunteersModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
