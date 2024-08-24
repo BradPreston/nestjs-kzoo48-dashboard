b531cd8c6b50c3defac2f22437417ccc506992ab
bradpreston <bap5393@gmail.com>
2024-08-24 16:59:38 -0400

    Import PrismaModule into the entries controller

--- src/entries/entries.controller.spec.ts
+++ src/entries/entries.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EntriesController } from './entries.controller';
 import { EntriesService } from './entries.service';
+import { PrismaModule } from '../../src/prisma/prisma.module';
 
 describe('EntriesController', () => {
   let controller: EntriesController;
@@ -9,6 +10,7 @@ describe('EntriesController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EntriesController],
       providers: [EntriesService],
+      imports: [PrismaModule],
     }).compile();
 
     controller = module.get<EntriesController>(EntriesController);
