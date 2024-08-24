bc7cddeca4b9e8717f7cbf2480ec05e2c5ad25bb
bradpreston <bap5393@gmail.com>
2024-08-24 15:18:04 -0400

    Update the entry mock data with bad data and multiple entries

--- test/mocks/entry.data.ts
+++ test/mocks/entry.data.ts
@@ -1,10 +1,70 @@
 import { Entry } from '@prisma/client';
 import { CreateEntryDto } from 'src/entries/dto/create-entry.dto';
+import { UpdateEntryDto } from 'src/entries/dto/update-entry.dto';
 
 export const mockEntry: Entry = {
   id: 1,
   firstName: 'John',
   lastName: 'Doe',
+  phone: '1234567890',
+  email: 'john.doe@email.com',
+  categoryId: 1,
+  teamName: "John's test team",
+  additionalEmails: [],
+  paymentId: null,
+  paid: false,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const badEntry: CreateEntryDto = {
+  lastName: 'Doe',
+  phone: '1234567890',
+  email: 'john.doe@email.com',
+  categoryId: 1,
+  teamName: "John's test team",
+  additionalEmails: [],
+  paid: false,
+  paymentId: null,
+};
+
+export const mockEntries: Entry[] = [
+  {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    phone: '1234567890',
+    email: 'john.doe@email.com',
+    categoryId: 1,
+    teamName: "John's test team",
+    additionalEmails: [],
+    paymentId: null,
+    paid: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 2,
+    firstName: 'Mary',
+    lastName: 'Miller',
+    phone: '0987654321',
+    email: 'mary.miller@test.com',
+    categoryId: 2,
+    teamName: "Mary's team",
+    additionalEmails: ['michael.motte@email.com'],
+    paymentId: 'abc123',
+    paid: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+export const mockUpdatedEntry: Entry = {
+  id: 1,
+  firstName: 'John updated',
+  lastName: 'Doe updated',
   phone: '1234567890',
   email: 'john.doe@email.com',
   categoryId: 1,
@@ -24,4 +84,18 @@ export const newEntry: CreateEntryDto = {
   categoryId: 1,
   teamName: "John's test team",
   additionalEmails: [],
+  paid: false,
+  paymentId: null,
+};
+
+export const updateEntry: UpdateEntryDto = {
+  firstName: 'John updated',
+  lastName: 'Doe updated',
+  phone: '1234567890',
+  email: 'john.doe@email.com',
+  categoryId: 1,
+  teamName: "John's test team",
+  additionalEmails: [],
+  paid: false,
+  paymentId: null,
 };
