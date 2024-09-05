15f5935a4c97e917b97c24d07e1e301f3e4fbb05
bradpreston <bap5393@gmail.com>
2024-09-04 21:00:00 -0400

    Generate REST resource for users

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { PaymentsModule } from './payments/payment.module';
 import { CategoriesModule } from './categories/categories.module';
 import { StatusesModule } from './statuses/statuses.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { AuthModule } from './auth/auth.module';
     CategoriesModule,
     StatusesModule,
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
