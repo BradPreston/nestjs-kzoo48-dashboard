90f1e7bbfaa84745e306204f0879c5ba990a240d
brad preston <bap5393@gmail.com>
2024-09-05 21:01:31 -0400

    Add method body to the findOne service method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -12,8 +15,22 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        email: true,
+        statusId: true,
+        createdAt: true,
+        updatedAt: true,
+        status: true,
+      },
+    });
+    if (!user) throw new NotFoundException(`No user with id "${id}" was found`);
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
